ff18203f9c0088ed76630db3a9a7a67b20cf5766
williepotgieter <willie.potgieter@kurtosys.com>
2021-04-25 13:35:22 +0200

    Completed part-2

---
+++ blockchain/block.go
@@ -0,0 +1,37 @@
+package blockchain
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+	Nonce    int
+}
+
+type BlockChain struct {
+	Blocks []*Block
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockchain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/williepotgieter/golang-blockchain
 
 go 1.16
-
-require rsc.io/quote/v3 v3.1.0
